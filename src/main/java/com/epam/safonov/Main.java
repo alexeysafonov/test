bbbb4abd302340c49924af61dd7f7f9a90d9d4fc
aleksei_safonov <HTCphilips66>
2014-04-03 20:20:53 +0400

    remove

---
+++ src/main/java/com/epam/safonov/Main.java
@@ -0,0 +1,29 @@
+package com.epam.safonov;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends HttpServlet {
+
+    /**
+     * mgjhnmhjmbmh,hb,
+     * @param request  jgvg,jkgmh,.
+     * @param response             p;k/ljk,hkjlj.
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    /**
+     * nkhjnngjhmnfnh
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
